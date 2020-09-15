b04a7474f02b06db7f101858547a338f764ba341
SunSeekerX <1647800606@qq.com>
2020-09-15 17:01:08 +0800

    feat: init

---
+++ src/test/java/cn/yoouu/UniPushyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yoouu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniPushyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
